4afcb60265d15432ce075b12ddd68263c9731b29
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2019-09-07 04:04:03 +0200

    Delete null_coalesing.php

--- null_coalesing.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "typed_properties.php";
-$users = [ new User(1, 'sara') , new User(2, 'ahmed')];
-// Ternary Operator
-//$response = count($users) ? $users : "NO Users !!";
-
-//Null Coalesing Operator
-$response = $users ?? "No Users !!";
-print_r($response);
